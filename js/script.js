1d585209c4fe2d7d0a64c092fc0a6441f5e21919
FriJ <egorik580@gmail.com>
2020-05-31 20:44:52 +0500

    create dots

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 
-window.curSetion = 1
+window.curSection = 1
 
 window.draw = () => {
     document.querySelectorAll('section').forEach(i => {
         i.classList.remove('active')
     })
-    document.querySelector(`section.el_${curSetion}`).classList.add('active')
+    document.querySelector(`section.el_${curSection}`).classList.add('active')
+
+    document.querySelectorAll('.dots .dot').forEach(i => {
+        i.classList.remove('active')
+    })
+    document.querySelector(`.dots .dot[page="${curSection}"]`).classList.add('active')
 }
 
 document.addEventListener('wheel', e => {
-    if (e.deltaY < 0 && curSetion > 1) {
-        curSetion -= 1
-    } else if (e.deltaY > 0 && curSetion < 6) {
-        curSetion += 1
+    if (e.deltaY < 0 && curSection > 1) {
+        curSection -= 1
+    } else if (e.deltaY > 0 && curSection < 6) {
+        curSection += 1
     }
     draw()
 })
+
+document.querySelectorAll('.dots .dot').forEach(item => {
+    item.addEventListener('click', e => {                
+        curSection = parseInt(e.target.attributes[1].value)
+        draw()
+    })
+})
+
 draw()
 
